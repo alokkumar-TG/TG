23fca37cfd9598d024a7d0dfc9bfa3c5daad1961
TestGrid Admin <info@testgrid.io>
2026-09-23 20:24:21 +0530

    Initial Version Commit

---
+++ compile-java-executable/constants.java
@@ -0,0 +1,7 @@
+public class constants {
+	public static int var_gfhfgh = 6;
+	public static String var_ra_Name = "Alok";
+	public static String var_ra_Age = "28";
+	public static String var_ra_Pen = "riffile";
+	public static String var_ra_water = "bottle";
+}
